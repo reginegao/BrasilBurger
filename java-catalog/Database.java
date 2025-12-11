e6c2037da8daa64f60309674553f50ea40f93f12
Regine gao <foukaregine@gmail.com>
2025-12-11 00:39:13 +0000

    Update Database.java

--- java-catalog/Database.java
+++ java-catalog/Database.java
@@ -3,13 +3,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    // Adresse de ta base
+    // Adresse de ta base Neon
     private static final String URL = "jdbc:postgresql://ep-empty-water-a40xqwma-pooler.us-east-1.aws.neon.tech:5432/BrasilBurgerDB?sslmode=require";
-    // Login / utilisateur
+    
+    // Nom de l'utilisateur
     private static final String USER = "neondb_owner";
-    // Mot de passe que tu viens de créer
+    
+    // Mot de passe que tu viens de générer
     private static final String PASSWORD = "npg_jiqRZl1n4UPh";
 
+    // Méthode pour se connecter à la base
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
